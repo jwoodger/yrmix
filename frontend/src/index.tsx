ee0d5902642023a8bbc57e4132585d3a6dbbb687
jwoodger <woodger.jack@gmail.com>
2023-04-17 17:36:17 -0400

    Create frontend project.

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+		<h1 className='header'>Hello!</h1>
+  </React.StrictMode>
+);
