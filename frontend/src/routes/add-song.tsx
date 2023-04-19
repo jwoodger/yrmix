f3ad3aa445bb7516ab08b611909923ebf3152a71
jwoodger <woodger.jack@gmail.com>
2023-04-18 23:04:26 -0400

    Add comments.

--- frontend/src/routes/add-song.tsx
+++ frontend/src/routes/add-song.tsx
@@ -4,23 +4,30 @@ import { useNavigate } from 'react-router-dom';
 
 import { SONG_BASE_URL } from '../constants';
 
+// Page to submit a specific song to the database.
 const AddSong: React.FC = () => {
 	const inputName = useRef<HTMLInputElement>(null);
 	const inputArtist = useRef<HTMLInputElement>(null);
 	const navigate = useNavigate();
 
+	// Submit the form.
 	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
 		event.preventDefault();
+		// Collect form data.
 		const formData = {
 			name: inputName.current?.value || '',
 			artist: inputArtist.current?.value || '',
 		};
 		try {
+			// Send form data to the backend.
 			const res = await fetch(SONG_BASE_URL, {
 				method: 'POST',
 				headers: {'Content-Type': 'application/json'},
 				body: JSON.stringify(formData)
 			});
+
+			// The backend will respond with the ID of the newly created song. We want
+			// to see that immediately after submitting the form.
 			const id = (await res.json()).id;
 			navigate(`/viewsong/${id}`);
 		} catch (err) {
